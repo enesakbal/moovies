febb578633f2b8812c72aad3a8582de71d95df93
Enes Akbal <enesakbal00@gmail.com>
2023-08-06 03:25:18 +0300

    feat: dependency injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import './src/injector.dart' as di;
 import 'src/config/router/app_router.dart';
+import 'src/core/theme/app_theme.dart';
+import 'src/presentation/bloc/search_movie/search_movie_bloc.dart';
 
 Future<void> main() async {
   /// Loading the environment variables.
@@ -21,18 +24,27 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(360, 800),
-      useInheritedMediaQuery: true,
-      builder: (context, _) {
-        return MaterialApp.router(
-          
-          //* router
-          routerDelegate: AutoRouterDelegate(di.injector<AppRouter>()),
-          routeInformationParser: di.injector<AppRouter>().defaultRouteParser(),
-          debugShowCheckedModeBanner: false,
-        );
-      },
+    return BlocProvider(
+      create: (context) => di.injector<SearchMovieBloc>(),
+      child: ScreenUtilInit(
+        designSize: const Size(360, 800),
+        useInheritedMediaQuery: true,
+        builder: (context, _) {
+          return MaterialApp.router(
+            theme: di.injector<AppTheme>().theme,
+
+            builder: (context, child) => MediaQuery(
+              data: MediaQuery.of(context).copyWith(textScaleFactor: 1),
+              child: child!,
+            ),
+
+            //* router
+            routerDelegate: AutoRouterDelegate(di.injector<AppRouter>()),
+            routeInformationParser: di.injector<AppRouter>().defaultRouteParser(),
+            debugShowCheckedModeBanner: false,
+          );
+        },
+      ),
     );
   }
 }
--- lib/src/injector.dart
+++ lib/src/injector.dart
@@ -1,13 +1,15 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 
+import 'config/router/app_router.dart';
 import 'core/network/dio_client.dart';
+import 'core/theme/app_theme.dart';
 import 'data/datasources/remote/remote_data_source.dart';
 import 'data/datasources/remote/remote_data_source_impl.dart';
 import 'data/repositories/repository_impl.dart';
 import 'domain/repositories/repository.dart';
 import 'domain/usecases/usecase.dart';
-
+import 'presentation/bloc/search_movie/search_movie_bloc.dart';
 
 /// Register all the dependencies that are needed for the app to run.
 final injector = GetIt.instance;
@@ -16,19 +18,24 @@ final injector = GetIt.instance;
 Future<void> init() async {
   injector
 
-        //* Network
-        ..registerLazySingleton<DioClient>(() => DioClient(Dio()))
+    //* Network
+    ..registerLazySingleton<DioClient>(() => DioClient(Dio()))
 
-        //* Datasources
-        ..registerLazySingleton<RemoteDataSource>(() => RemoteDataSourceImpl(injector()))
+    //* Datasources
+    ..registerLazySingleton<RemoteDataSource>(() => RemoteDataSourceImpl(injector()))
 
-        //* Repository
-        ..registerLazySingleton<Repository>(() => RepositoryImpl(injector()))
+    //* Repository
+    ..registerLazySingleton<Repository>(() => RepositoryImpl(injector()))
 
-        //* Usecase
-        ..registerLazySingleton(() => Usecases(injector()))
+    //* Usecase
+    ..registerLazySingleton(() => Usecases(injector()))
 
-        //* BLoC
+    //* BLoC
+    ..registerFactory(() => SearchMovieBloc(injector()))
 
-      ;
+    //* Router
+    ..registerSingleton<AppRouter>(AppRouter())
+
+    //* Theme
+    ..registerSingleton<AppTheme>(AppTheme());
 }
