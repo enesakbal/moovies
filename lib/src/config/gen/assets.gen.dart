7c9e676219c12f8e0e95e4f8983978f54188764f
Enes Akbal <enesakbal00@gmail.com>
2023-08-06 03:20:08 +0300

    ref: some refs

--- lib/src/config/gen/assets.gen.dart
+++ lib/src/config/gen/assets.gen.dart
@@ -30,10 +30,22 @@ class $AssetsIconsGen {
   List<SvgGenImage> get values => [code, error, search, star];
 }
 
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  /// File path: assets/images/app_icon.png
+  AssetGenImage get appIcon =>
+      const AssetGenImage('assets/images/app_icon.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [appIcon];
+}
+
 class Assets {
   Assets._();
 
   static const $AssetsIconsGen icons = $AssetsIconsGen();
+  static const $AssetsImagesGen images = $AssetsImagesGen();
 }
 
 class AssetGenImage {
