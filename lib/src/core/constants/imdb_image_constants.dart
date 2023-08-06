d3ef0a797dc365264018ac9178e686609a744043
Enes Akbal <enesakbal00@gmail.com>
2023-08-06 03:23:58 +0300

    ref: refactored TMDBImageConstants

--- lib/src/core/constants/imdb_image_constants.dart
+++ lib/src/core/constants/imdb_image_constants.dart
@@ -1,3 +1,3 @@
 class TMDBImageConstants {
-  static const poster = 'https://image.tmdb.org/t/p/w220_and_h330_face/';
+  static const poster = 'https://image.tmdb.org/t/p/w220_and_h330_face/{poster_path}';
 }
